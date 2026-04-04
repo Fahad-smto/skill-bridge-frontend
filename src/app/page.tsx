5ec441ea23cdf6700d727ab9161f571f53ccd005
Fahad-smto <simanto.smto.prog@gmail.com>
2026-04-04 23:59:45 +0600

    removing code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Button variant="outline">Click me</Button>
+      
     </div>
   );
 }
