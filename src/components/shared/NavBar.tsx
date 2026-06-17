887024bc982c9538ca26de0b5674fb493a1cea5f
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-17 22:47:30 +0600

    color changed

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -103,7 +103,7 @@ export default function Navbar() {
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2 group">
             <GraduationCap className="w-6 h-6 text-black group-hover:scale-110 transition-transform" />
-            <span className="font-bold text-xl text-black bg-clip-text   group-hover:from-black group-hover:to-gray-800 transition-colors">
+            <span className="font-bold text-xl italic text-black bg-clip-text   group-hover:from-black group-hover:to-gray-800 transition-colors">
               SkillBridge
             </span>
           </Link>
