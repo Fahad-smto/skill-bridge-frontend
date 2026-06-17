069dde5aa4d438689b33e77b07e7e327f65b415e
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-17 22:43:44 +0600

    hover added

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -102,7 +102,7 @@ export default function Navbar() {
 
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2 group">
-            <GraduationCap className="w-6 h-6 text-blue-600 group-hover:scale-110 transition-transform" />
+            {/* <GraduationCap className="w-6 h-6 text-blue-600 group-hover:scale-110 transition-transform" /> */}
             <span className="font-bold text-xl bg-gradient-to-r from-blue-600 to-blue-800 bg-clip-text text-transparent">
               SkillBridge
             </span>
