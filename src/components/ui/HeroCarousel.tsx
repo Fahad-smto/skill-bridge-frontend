9fe683d9d526c4a02e218b4a488b375e4134bc3e
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-18 23:25:57 +0600

    color changed

--- src/components/ui/HeroCarousel.tsx
+++ src/components/ui/HeroCarousel.tsx
@@ -4,7 +4,7 @@ import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import { useEffect, useState, useCallback } from 'react'
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
+// import { Button } from '@/components/ui/button'
 import { ArrowRight, ChevronLeft, ChevronRight } from 'lucide-react'
 
 const slides = [
