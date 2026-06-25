1262bf663c7aa7bfd59221fcfd2fbb79c1291008
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-25 22:22:36 +0600

    query added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-      
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
